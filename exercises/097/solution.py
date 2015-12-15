2a7cc68658f5f1b537f1dfa79c72bb5e6698b3e5
Emelynegdu <emelyne.gaudichau@cri-paris.org>
2015-12-15 12:25:11 +0100

    test

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -1,5 +1,6 @@
 bob = ['II', 'IV', 'II', 'XIX', 'XV', 'IV', 'II']
 alice = ['IV', 'III', 'II', 'XX', 'II', 'XX']
+silvester = ['XVIII', 'XIX', 'III', 'I', 'III', 'XVIII']
 def love_meet(bob, alice):
     for i in set(bob):
         for j in set(alice):
@@ -8,4 +9,16 @@ def love_meet(bob, alice):
 
 
 
-love_meet(bob, alice)
+
+banane = []
+def affair_meet(bob, alice, silvester):
+    for i in set(bob):
+        for j in set(alice):
+            for k in set(silvester):
+                if k == i:
+                    if k != j:
+                        if k not in banane:
+                            banane.append(k)
+                            print(k)
+
+affair_meet(bob, alice, silvester)
